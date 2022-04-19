49f35bda72534e1cc08604c0d18246be27984c4d
A00446098 <James.Alexander.Kyasaram@smu.ca>
2022-04-19 15:37:36 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,32 @@
 from collections import namedtuple
-import altair as alt
+
 import math
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+import pandas as pd
+import requests
+from datetime import datetime as dt
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
 
-    points_per_turn = total_points / num_turns
+st.title("Bitcoin prices")
+days=st.slider('No Of days',min_value=1,max_value=365,value=90)
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+currency=st.radio('Currency', ('cad','usd','inr'))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+s='https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency='+currency+'&days='+str(days-1)+'&interval=daily'
+r = requests.get(s, auth=('user', 'pass'))
+data=r.json()
+
+
+
+d=pd.DataFrame(data=data['prices'],columns=['date','Prices'])
+d['date']=pd.to_datetime(d['date'],unit='ms')
+d['date']=d['date'].apply(lambda x:x.date())
+d.set_index('date',inplace=True)
+
+st.line_chart(d)
+
+st.write("Average price during this time was ",d['Prices'].mean()," ",currency)
